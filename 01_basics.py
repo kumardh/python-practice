eb37ecdefae097e92a01b49f03d6777da51225f7
Dhananjay kumar <dhananjay.kumar@neudesic.com>
2019-09-12 12:47:36 +0530

    Updated

--- 01_basics.py
+++ 01_basics.py
@@ -1,16 +1,51 @@
 # How to print
 x=10
 y=20
-print(x+y)
+print(x+y) # 30
+
+# By default its always printed in new line.
+x=10
+y=20
+print(x+y) # 30
+print(x*y) # 200
+
+# You can't do like this. Below will return TypeError: must be str, not int
+# print("Number is : " + 30)
+
+# Change number to str as below.
+print("Number is: " + str(30)) # Number is: 30
+
+# In case you need to print in same line then use "end" as below
+x=10
+y=20
+print(x+y, end=":")
+print(x*y)
+# 30:200
+
+# Read input from User. Note the entered value is of type str. So use int cast if you need to do some mathematical operation.
+x = input("Enter number: ")
+print(type(x)) # <class 'str'>
+print(int(x)+ 100) # 110
+
+# Handle User Input
+try :
+  x = int(input("Enter number 1: ")) # enter 10
+except ValueError:
+  x = 0
+try :
+  y = int(input("Enter number 2: ")) # enter 20
+except ValueError:
+  y = 0
+print("sum: " + str(x+y)) # sum: 30
 
 # A simple function
 def sum(x,y):
     return x+y
-print(sum(5,2))
+print(sum(5,2)) # 7
 
 # Getting type of a value
 f = 101;
-print(type(f))
+print(type(f)) # <class 'int'>
 
 # Global vs.local variables in functions
 def someFunction():
@@ -21,36 +56,25 @@ someFunction()
 print(f)
 
 # Don’t use global keyword unless you know what you are doing. In case you need to retun multiple values return tuple as below -
-# Correct method
+# Recommended method
 def profile():
   name = "Danny"
   age = 30
   return name, age
 profile_name, profile_age = profile()
-print(profile_name)
-print(profile_age)
+print(profile_name) # Danny
+print(profile_age) # 30
 
-# Incorrect method
+# Not recommended method
 def profile():
   global name, age
   name = "Danny"
   age = 30
 profile()
-print(name)
-print(age)
+print(name) # Danny
+print(age) # 30
 
-# Reading User input
-try :
-  x = int(input("Enter number 1: "))
-except ValueError:
-  x = 0
-try :
-  y = int(input("Enter number 2: "))
-except ValueError:
-  y = 0
-print("sum: " + str(x+y))
-
-# use while loop
+# use while loop to force user to enter correct number.
 def ReadNumber():
   loopCondition = True
   while loopCondition:
--- 06_type.py
+++ 06_type.py
@@ -1,22 +1,22 @@
 #Check if "Hello" is one of the types described in the type tuple
-print(isinstance("Hello", (float, int, str, list, dict, tuple)))
+print(isinstance("Hello", (float, int, str, list, dict, tuple))) # True
 
 dic = {"a":1, "b":2}
-print(type(dic))
-print(isinstance(dic,(int,str)))
-print(isinstance(dic,dict))
-print(isinstance(dic,(int,str,dict)))
+print(type(dic)) # <class 'dict'>
+print(isinstance(dic,(int,str))) # False
+print(isinstance(dic,dict)) # True
+print(isinstance(dic,(int,str,dict))) # True
 
 lst = ["a","b"]
-print(type(lst))
-print(isinstance(lst,(list)))
-print(isinstance(lst,(list, int)))
-print(isinstance(lst,(list, str)))
-print(isinstance(lst,str))
+print(type(lst)) # <class 'list'>
+print(isinstance(lst,(list))) # True
+print(isinstance(lst,(list, int))) # True
+print(isinstance(lst,(list, str))) # True
+print(isinstance(lst,str)) # False
 
 class myObj:
   name = "John"
 
 y = myObj()
-print(type(lst))
-print(isinstance(y, myObj))
+print(type(lst)) # <class 'list'>
+print(isinstance(y, myObj)) # True
